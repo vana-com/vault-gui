0654eead1e9d47e7c9dccaa7d35fa3deca2a63ea
Kahtaf Alam <kahtaf@gmail.com>
2022-07-19 10:09:54 -0400

    Remove scripts symlink

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-/* eslint-disable no-param-reassign */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer: _isServer }) => {
-    config.resolve.symlinks = false; // Ignores scripts/ symlink
-    return config;
-  }
 }
 
 module.exports = nextConfig
