52f3a22b9a383aac87c58cbbb6c270bc4a4bd4c0
Kahtaf Alam <kahtaf@gmail.com>
2022-07-19 09:55:30 -0400

    Ignore scripts/ symlink

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-param-reassign */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer: _isServer }) => {
+    config.resolve.symlinks = false; // Ignores scripts/ symlink
+    return config;
+  }
 }
 
 module.exports = nextConfig
