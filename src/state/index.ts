da96bd54db8049728c7d178f64ea6c6dbe00ab14
Zach Hay <zach.hay@hey.com>
2022-07-21 11:28:55 -0700

    add web3AuthUserInfoAtom

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,14 +1,20 @@
+import { UserInfo } from "@web3auth/base";
 import { atom } from "jotai";
 
 interface User {
   id: string;
 }
 
-const userAtom = atom<User | undefined>({
-  id: "user123",
-});
+const userAtom = atom<User | undefined>(undefined);
 const idTokenAtom = atom<string | undefined>(undefined);
 const appPubKeyAtom = atom<string | undefined>(undefined);
 const hasuraTokenAtom = atom<string | undefined>(undefined);
+const web3AuthUserInfoAtom = atom<Partial<UserInfo> | undefined>(undefined);
 
-export { appPubKeyAtom, hasuraTokenAtom, idTokenAtom, userAtom };
+export {
+  appPubKeyAtom,
+  hasuraTokenAtom,
+  idTokenAtom,
+  userAtom,
+  web3AuthUserInfoAtom,
+};
