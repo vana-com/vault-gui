e06eef48cc5276407cba3cff5de7372c80aa837b
Callum Flack <callum@vana.org>
2022-07-22 16:16:48 +1000

    Link: allow size inheritance

--- src/components/system/Link/Link.tsx
+++ src/components/system/Link/Link.tsx
@@ -1,10 +1,13 @@
-import tw, { styled } from "twin.macro";
+import tw, { css, styled } from "twin.macro";
 
 export type LinkProps = {
   underline?: boolean;
 };
 
 export const Link = styled.a(({ underline = true }: LinkProps) => [
-  tw`outline-none decoration-current decoration-[0.075em] underline-offset-[0.09375em]`,
+  tw`outline-none decoration-current decoration-[0.05em] underline-offset-[0.15em]`,
   underline && tw`underline`,
+  css`
+    font-size: inherit;
+  `,
 ]);
