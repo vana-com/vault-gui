4d47e208641c34148d000de83ecdc34b216fc214
Callum Flack <callum@vana.org>
2022-07-25 13:27:49 +1000

    add confim dialog to Logout

--- src/components/UserAccess/Logout.tsx
+++ src/components/UserAccess/Logout.tsx
@@ -1,8 +1,17 @@
 import { useAtom } from "jotai";
 import { useState } from "react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import tw from "twin.macro";
 
-import { Button } from "src/components";
-import { CarbonArrowRight } from "src/components/Icons";
+import {
+  Button,
+  DialogClose,
+  DialogModal,
+  Group,
+  Stack,
+  Text,
+} from "src/components";
+import { CarbonSecurity } from "src/components/Icons";
 import config from "src/config";
 import {
   hasuraTokenAtom,
@@ -34,16 +43,44 @@ const Logout = () => {
   };
 
   return (
-    <Button
-      type="button"
-      variant="outline"
-      size="md"
-      suffix={<CarbonArrowRight />}
-      isLoading={isLoading}
-      onClick={logOut}
+    <DialogModal
+      buttonSlot={
+        <Button
+          type="button"
+          variant="outline"
+          size="md"
+          prefix={<CarbonSecurity />}
+        >
+          Log Out
+        </Button>
+      }
     >
-      Log Out
-    </Button>
+      <Stack tw="gap-1 justify-center">
+        <Text color="label" variant="title2" tw="text-center">
+          Are you sure?
+        </Text>
+        <Text color="labelSecondary" tw="text-center">
+          Confirm your secure logout
+        </Text>
+
+        <Group tw="gap-4 pt-7 justify-center">
+          <Button
+            variant="solid"
+            size="lg"
+            prefix={<CarbonSecurity />}
+            isLoading={isLoading}
+            onClick={logOut}
+          >
+            Log Out
+          </Button>
+          <DialogClose asChild>
+            <Button variant="outline" size="lg">
+              Not yet
+            </Button>
+          </DialogClose>
+        </Group>
+      </Stack>
+    </DialogModal>
   );
 };
 
