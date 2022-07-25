b16e6bba4b345993e8af6c20a1e2c0701663b609
Callum Flack <callum@vana.org>
2022-07-26 08:10:36 +1000

    remove random id on Spinner to rpevent React hydration issue

--- src/components/system/Spinner/Spinner.tsx
+++ src/components/system/Spinner/Spinner.tsx
@@ -1,13 +1,13 @@
 import { keyframes } from "@emotion/react";
-import React, { useMemo } from "react";
+import React from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw, { css } from "twin.macro";
 
-const useRandomId = (prefix: string) =>
-  useMemo(
-    () => `${prefix}_${Math.round(Math.random() * 1_000_000_000)}`,
-    [prefix],
-  );
+// const useRandomId = (prefix: string) =>
+//   useMemo(
+//     () => `${prefix}_${Math.round(Math.random() * 1_000_000_000)}`,
+//     [prefix],
+//   );
 
 // you must close the rule with `;` or Emotion won't understand it
 const spin = keyframes`
@@ -26,35 +26,29 @@ export const iconPath = css({
 });
 
 export const Spinner = ({
+  id = "spinner-01",
   height = 21,
   width = 21,
 }: {
+  id?: string;
   width?: string | number;
   height?: string | number;
-}) => {
-  const id = useRandomId("spinner");
+}) => (
+  // const id = useRandomId("spinner");
 
-  return (
-    <svg
-      css={icon}
-      fill="none"
-      height={height}
-      viewBox="0 0 21 21"
-      width={width}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <clipPath id={id}>
-        <path d="M10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18C11.3284 18 12 18.6716 12 19.5C12 20.3284 11.3284 21 10.5 21C4.70101 21 0 16.299 0 10.5C0 4.70101 4.70101 0 10.5 0C16.299 0 21 4.70101 21 10.5C21 11.3284 20.3284 12 19.5 12C18.6716 12 18 11.3284 18 10.5C18 6.35786 14.6421 3 10.5 3Z" />
-      </clipPath>
-      <foreignObject
-        clipPath={`url(#${id})`}
-        height="21"
-        width="21"
-        x="0"
-        y="0"
-      >
-        <div css={iconPath} />
-      </foreignObject>
-    </svg>
-  );
-};
+  <svg
+    css={icon}
+    fill="none"
+    height={height}
+    viewBox="0 0 21 21"
+    width={width}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <clipPath id={id}>
+      <path d="M10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18C11.3284 18 12 18.6716 12 19.5C12 20.3284 11.3284 21 10.5 21C4.70101 21 0 16.299 0 10.5C0 4.70101 4.70101 0 10.5 0C16.299 0 21 4.70101 21 10.5C21 11.3284 20.3284 12 19.5 12C18.6716 12 18 11.3284 18 10.5C18 6.35786 14.6421 3 10.5 3Z" />
+    </clipPath>
+    <foreignObject clipPath={`url(#${id})`} height="21" width="21" x="0" y="0">
+      <div css={iconPath} />
+    </foreignObject>
+  </svg>
+);
