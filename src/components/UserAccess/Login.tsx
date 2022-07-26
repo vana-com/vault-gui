73e5254843fa5668a9aa3e50c500209aed85b257
Callum Flack <callum@vana.org>
2022-07-26 11:39:12 +1000

    Login: add error Toast

--- src/components/UserAccess/Login.tsx
+++ src/components/UserAccess/Login.tsx
@@ -5,11 +5,12 @@ import {
 } from "@web3auth/base";
 import { Web3Auth } from "@web3auth/web3auth";
 import { useAtom } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from "twin.macro";
 
-import { Button } from "src/components";
+import { Button, Link } from "src/components";
+import { ToastDefault } from "src/components/Toast";
 import config from "src/config";
 import {
   hasuraTokenAtom,
@@ -29,6 +30,7 @@ const Login = () => {
   const setWalletProvider = useAtom(web3AuthWalletProviderAtom)[1];
   const [walletAdapter, setWalletAdapter] = useAtom(web3AuthAdapterAtom);
   const [idToken, setIdToken] = useAtom(idTokenAtom);
+  const [loginError, setLoginError] = useState(false);
 
   // get Hasura user object
   useEffect(() => {
@@ -108,19 +110,38 @@ const Login = () => {
       setWalletProvider(web3AuthProvider);
     } catch (error: any) {
       console.log(error.toString());
+      setLoginError(true);
     }
   };
 
   return (
-    <Button
-      type="button"
-      variant="solid"
-      size="xl"
-      css={tw`min-w-[155px]`}
-      onClick={logIn}
-    >
-      Log In
-    </Button>
+    <>
+      <Button
+        type="button"
+        variant="solid"
+        size="xl"
+        css={tw`min-w-[220px] max-w-[220px]`}
+        onClick={logIn}
+      >
+        Log In
+      </Button>
+
+      {/* TOAST for any errors */}
+      <ToastDefault
+        open={loginError}
+        onOpenChange={setLoginError}
+        duration={12000}
+        variant="error"
+        title="Something went wrong"
+        content={
+          <>
+            Please{" "}
+            <Link href="mailto:support@vanahelp.zendesk.com">email us</Link>{" "}
+            with details of your login attempt.
+          </>
+        }
+      />
+    </>
   );
 };
 
