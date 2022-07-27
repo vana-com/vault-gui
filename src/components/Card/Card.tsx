6c87344e8bd742c4e876133a12d90d58be2cc2ea
Callum Flack <callum@vana.org>
2022-07-27 15:54:39 +1000

    refactor Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,23 +1,26 @@
-import tw, { styled } from "twin.macro";
+import tw from "twin.macro";
 
-interface Props {
+interface InnerProps {
+  small?: boolean;
+}
+
+const innerStyle = ({ small }: InnerProps) => [
+  tw`flex flex-1 rounded-md`,
+  small ? tw`p-2.5 lg:p-5` : tw`p-4 lg:p-8`,
+];
+
+interface CardProps extends InnerProps {
   children: React.ReactNode;
 }
 
-const Root = styled.div([
-  tw`
-    flex flex-col rounded-lg shadow-lg
+const Card = ({ children, small }: CardProps) => (
+  <div
+    tw="flex flex-col rounded-lg shadow-lg
     min-h-[195px] h-full overflow-hidden
-    bg-backgroundElevated
-  `,
-]);
-
-const Inner = styled.div([tw`flex flex-1 p-4 rounded-md lg:p-8`]);
-
-const Card = ({ children }: Props) => (
-  <Root>
-    <Inner>{children}</Inner>
-  </Root>
+    bg-backgroundElevated"
+  >
+    <div css={innerStyle({ small })}>{children}</div>
+  </div>
 );
 
 export { Card };
