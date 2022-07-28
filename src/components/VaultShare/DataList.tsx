892d4df7edef2f68e0ba3ef93c915ea162f2e9b5
Callum Flack <callum@vana.org>
2022-07-28 10:36:26 +1000

    extract permissions list copy into a test array

--- src/components/VaultShare/DataList.tsx
+++ src/components/VaultShare/DataList.tsx
@@ -3,6 +3,7 @@ import tw from "twin.macro";
 
 import { Group, Stack, Text } from "src/components";
 import { CarbonSecurity } from "src/components/Icons";
+import { testPermissionList } from "src/data";
 
 const DataListItem = ({ children }: Props) => (
   <Group tw="items-center gap-2">
@@ -17,13 +18,10 @@ interface Props {
 
 const DataList = ({ children }: Props) => (
   <Stack tw="rounded-lg border border-separator p-4 gap-3">
-    <DataListItem>
-      Hey I&apos;m a data share item you&apos;re confirming access to
-    </DataListItem>
-    <DataListItem>Hey don&apos;t forget about me baby</DataListItem>
-    <DataListItem>
-      Hey I&apos;m also data share item you&apos;re confirming access to
-    </DataListItem>
+    {testPermissionList.map((item, index) => (
+      // eslint-disable-next-line react/no-array-index-key
+      <DataListItem key={index}>{item}</DataListItem>
+    ))}
     {children}
   </Stack>
 );
---
+++ src/data/sharePermissions.ts
@@ -0,0 +1,5 @@
+export const testPermissionList = [
+  "Hey I'm a data share item you're confirming access to",
+  "Hey I'm also a data share item you're confirming access to",
+  "Hey don't forget about me baby",
+];
