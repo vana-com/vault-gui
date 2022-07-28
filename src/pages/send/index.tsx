4c8c9e96e331a3777a3ee0d975cd9545a306f720
Callum Flack <callum@vana.org>
2022-07-28 17:17:45 +1000

    Send index: add some UI states

--- src/pages/send/index.tsx
+++ src/pages/send/index.tsx
@@ -4,18 +4,26 @@ import { useState } from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from "twin.macro";
 
-import { Container, TitleAndMetaTags } from "src/components";
-import { PermissionContract, PermissionList } from "src/components/VaultShare";
+import { Flex, Login, TitleAndMetaTags } from "src/components";
+import {
+  NoModuleMessage,
+  PermissionContract,
+  PermissionList,
+  VaultSharePage,
+} from "src/components/VaultShare";
+import { useLogin } from "src/hooks";
 import { web3AuthUserInfoAtom } from "src/state";
 import { runDataQueryPipeline } from "src/utils";
 
 // Sharing API Page to be opened in 3rd-party website as a popup
 const SendPage: NextPage = () => {
+  const { logIn, loginError, setLoginError } = useLogin();
   const [web3AuthUserInfo] = useAtom(web3AuthUserInfoAtom);
   const [hasUserAcceptedSharingRequest, setHasUserAcceptedSharingRequest] =
     useState(false);
 
   const dummySQLQuery = "select * from instagram_interests";
+  const testAccessor = "Dall•e";
 
   const onDataRequestApproval = async () => {
     setHasUserAcceptedSharingRequest(true);
@@ -24,32 +32,66 @@ const SendPage: NextPage = () => {
     console.log("dataToSend", dataToSend);
   };
 
+  // STATE TESTS
+  // const testUserAvailable = true;
+  const testUserWithoutData = false;
+
+  // Login state: prior to authenticated store user
   if (!web3AuthUserInfo) {
-    // TODO: Have user go through login flow
+    return (
+      <>
+        <TitleAndMetaTags
+          color="black"
+          title="Login to share your Vault | Vana"
+        />
+        <VaultSharePage lede="Login to allow Vault access">
+          <Flex tw="w-full items-center justify-center">
+            <Login
+              logIn={logIn}
+              loginError={loginError}
+              setLoginError={setLoginError}
+            />
+          </Flex>
+        </VaultSharePage>
+      </>
+    );
   }
 
+  // No Module state: Logged-in but without a Module
+  if (testUserWithoutData) {
+    return (
+      <>
+        <TitleAndMetaTags color="black" title="Add data to your Vault | Vana" />
+        <VaultSharePage
+          accessDenied
+          heading="No Vault data"
+          lede={`${testAccessor} can't access any Vault data`}
+        >
+          <NoModuleMessage />
+        </VaultSharePage>
+      </>
+    );
+  }
+
+  // Confirmed state: sending data
   if (hasUserAcceptedSharingRequest) {
     <div>Sending your data... Cool animation</div>;
   }
 
+  // Permissions contract state: requesting permissions
   return (
     <>
-      <TitleAndMetaTags color="black" title="Send your Vault data | Vana" />
-
-      <div tw="min-h-screen flex items-center justify-center">
-        <Container tw="relative" size="lg">
-          <PermissionContract
-            accessor="Dall•e"
-            onAccept={onDataRequestApproval}
-            onDeny={() => {
-              // TODO: close popup or prompt the user to close popup
-              console.log("close popup");
-            }}
-          >
-            <PermissionList query={dummySQLQuery} />
-          </PermissionContract>
-        </Container>
-      </div>
+      <TitleAndMetaTags color="black" title="Share your Vault data | Vana" />
+      <PermissionContract
+        accessor={testAccessor}
+        onAccept={onDataRequestApproval}
+        onDeny={() => {
+          // TODO: close popup or prompt the user to close popup
+          console.log("close popup");
+        }}
+      >
+        <PermissionList query={dummySQLQuery} />
+      </PermissionContract>
     </>
   );
 };
