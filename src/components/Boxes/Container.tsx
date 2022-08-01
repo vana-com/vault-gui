eba012955faec1169201aa10053e568065ae6cc3
Callum Flack <callum@vana.org>
2022-08-01 13:39:39 +1000

    Update Container.tsx

--- src/components/Boxes/Container.tsx
+++ src/components/Boxes/Container.tsx
@@ -1,8 +1,8 @@
 import tw, { styled } from "twin.macro";
 
 const containerSize = {
-  sm: tw`sm:max-w-2xl`,
-  lg: tw`sm:max-w-3xl`,
+  sm: tw`sm:max-w-2xl`, // 672px
+  lg: tw`sm:max-w-3xl`, // 768px
 } as const;
 
 type Size = keyof typeof containerSize;
