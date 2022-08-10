ae6dd78150d1ba7a1f17a2b13b228d6eb4ee33a3
Callum Flack <callum@vana.org>
2022-08-10 17:37:45 +1000

    Logout: fix with User Context

--- src/components/UserAccess/Logout.tsx
+++ src/components/UserAccess/Logout.tsx
@@ -1,59 +1,23 @@
 import { Icon } from "@iconify/react";
-import { useAtom } from "jotai";
-import { useState } from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from "twin.macro";
 
 import { Spinner, Text, WithIcon } from "src/components";
 import { navLinkStyle } from "src/components/Nav/Nav.css";
-import {
-  hasuraTokenAtom,
-  idTokenAtom,
-  userAtom,
-  userWalletAddressAtom,
-  web3AuthUserInfoAtom,
-  web3AuthWalletProviderAtom,
-} from "src/state";
-import { logOut as logOutUtil } from "src/utils";
 
+import { useUserContext } from "./UserContext";
+
+/* TODO: Callum will replace LoginButton within this component after `development` merge */
 const Logout = () => {
-  const [, setWeb3AuthUserInfo] = useAtom(web3AuthUserInfoAtom);
-  const [, setUser] = useAtom(userAtom);
-  const [, setUserWalletAddress] = useAtom(userWalletAddressAtom);
-  const [, setIdToken] = useAtom(idTokenAtom);
-  const [, setHasuraToken] = useAtom(hasuraTokenAtom);
-  const [, setWalletProvider] = useAtom(web3AuthWalletProviderAtom);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const logOut = async () => {
-    setIsLoading(true);
-    logOutUtil(
-      setWalletProvider,
-      setUserWalletAddress,
-      setWeb3AuthUserInfo,
-      setUser,
-      setHasuraToken,
-      setIdToken,
-    );
-  };
+  const { logoutUser, isLoading } = useUserContext();
 
   return (
-    // <Button
-    //   type="button"
-    //   variant="ghost"
-    //   size="md"
-    //   prefix={<Icon icon="heroicons-solid:logout" />}
-    //   isLoading={isLoading}
-    //   onClick={logOut}
-    // >
-    //   Log Out
-    // </Button>
     <Text
       as="button"
       variant="base"
       weight="medium"
       css={[navLinkStyle, tw`px-3 text-label`]}
-      onClick={logOut}
+      onClick={logoutUser}
     >
       <WithIcon
         prefix={
--- src/components/UserAccess/index.ts
+++ src/components/UserAccess/index.ts
@@ -1,3 +1,4 @@
 export * from "./LoginButton";
 export * from "./LoginLayout";
+export * from "./Logout";
 export * from "./LogoutButton";
