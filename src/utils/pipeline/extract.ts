221d9c5fd5dfb9d17e18e44457ba37606ebad945
Joseph Csoti <joseph@vana.org>
2022-08-10 12:23:44 -0400

    Run DataPipeline as a non-worker (CU-3r2jbhw) (#63)
    * refactor
    
    * moved params into a type
    
    * PR review changes
    
    * function return types

---
+++ src/utils/pipeline/extract.ts
@@ -0,0 +1,15 @@
+import { zipToSQLiteInstance } from "@corsali/userdata-extractor";
+
+/**
+ * Extracts a .zip file into a sqlite database
+ * @param data File to extract
+ * @returns sqlite database
+ */
+const extractData = async (data: File, serviceName: string) => {
+  // Enable webworkers inside zipjs
+  const extracted = await zipToSQLiteInstance(serviceName, data, true);
+
+  return extracted;
+};
+
+export { extractData };
---
+++ src/utils/pipeline/query.ts
@@ -0,0 +1,24 @@
+interface SQLiteQueryResult {
+  queryString: string;
+  queryResult: string[];
+}
+
+/**
+ * Runs a query on a sqlite database
+ * @param data sqlite database
+ * @param query SQL query
+ * @returns matching rows
+ */
+const queryData = async (
+  db: any,
+  query: string,
+): Promise<SQLiteQueryResult[]> => {
+  if (!query) throw new Error("No query provided");
+
+  const queryResults: SQLiteQueryResult[] = await db.runQuery(query);
+
+  return queryResults;
+};
+
+export { queryData };
+export type { SQLiteQueryResult };
