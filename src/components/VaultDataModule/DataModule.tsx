af572947cbd37914ad02259185880e472213c50c
Callum Flack <callum@vana.org>
2022-08-10 14:56:24 +1000

    DataModule: rename module > block, add DL icon

--- src/components/VaultDataModule/DataModule.tsx
+++ src/components/VaultDataModule/DataModule.tsx
@@ -1,11 +1,14 @@
+import { Icon } from "@iconify/react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from "twin.macro";
 
 import {
+  Button,
   CardHeadingModule,
   DataCard,
   DataModuleDetail,
   DialogDrawer,
+  Group,
   Stack,
 } from "src/components";
 import { ptBreadcrumbs } from "src/components/system/Styles";
@@ -23,7 +26,7 @@ const DataModule = ({ module, moduleName, children }: Props) => (
     <Stack css={[tw`gap-2 px-inset pb-insetAlmost bg-neutral`, ptBreadcrumbs]}>
       <CardHeadingModule
         name={moduleName}
-        heading={`${moduleName} data module`}
+        heading={`${moduleName} data block`}
         variant="title1"
       />
       <DataModuleDetail label="Module ID" alignByWidth={false}>
@@ -34,7 +37,21 @@ const DataModule = ({ module, moduleName, children }: Props) => (
       <Stack tw="gap-2">
         {/* TODO: pass module details when avalable */}
         <DataModuleDetail label="File name">
-          {testModules[0].fileName}
+          <Group tw="gap-1 items-center">
+            {testModules[0].fileName}
+            {/* TODO: provide safe download link */}
+            <div tw="relative">
+              <Button
+                // as="a"
+                // target="_blank"
+                // href={module.urlToData}
+                variant="icon"
+                tw="text-label absolute top-1/2 transform -translate-y-1/2"
+              >
+                <Icon icon="carbon:download" />
+              </Button>
+            </div>
+          </Group>
         </DataModuleDetail>
         <DataModuleDetail label="Size">
           {testModules[0].fileSize}
