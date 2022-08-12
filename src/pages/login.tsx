777e6aff9ccf97e9e2e621c2283732ae3da8eab0
Callum Flack <callum@vana.org>
2022-08-12 12:40:55 +1000

    Login page: redirects
    * useEffect redirects based on side effects of isAuthenticated & origin params
    * direct redirect based on already-logged-in user access

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,43 +1,79 @@
 import { Icon } from "@iconify/react";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from "twin.macro";
 
 import {
   Center,
-  LayoutApp,
+  LayoutAppNoAside,
   LayoutCanvas,
   LayoutCanvasPattern,
   LoginButton,
   Stack,
   Text,
+  TitleAndMetaTags,
+  useUserContext,
   WithIcon,
 } from "src/components";
 
-const LoginPage: NextPage = () => (
-  <LayoutApp>
-    <LayoutCanvas>
-      <LayoutCanvasPattern />
-      <Center tw="min-h-[300px] relative">
-        <Stack tw="gap-5 items-center">
-          <Stack tw="gap-1 items-center">
-            <Text variant="base" weight="semibold">
-              Log in or create a new account
-            </Text>
-            <Text
-              variant="note"
-              tw="text-labelSecondary flex items-center gap-1"
-            >
-              <WithIcon prefix={<Icon icon="carbon:idea" />}>
-                If you have an account, use the same login method
-              </WithIcon>
-            </Text>
-          </Stack>
-          <LoginButton />
-        </Stack>
-      </Center>
-    </LayoutCanvas>
-  </LayoutApp>
-);
+const LoginPage: NextPage = () => {
+  const router = useRouter();
+  const { isAuthenticated } = useUserContext();
+
+  // After logging in, useEffect to redirect the newly logged-in user.
+  useEffect(() => {
+    // If there's an origin param, redirect the user back to that origin
+    if (router.query?.origin && isAuthenticated) {
+      router.push(
+        decodeURIComponent(router.query.origin.toString()),
+        undefined,
+        {
+          shallow: true,
+        },
+      );
+    }
+    // If there's no origin param, redirect the user home
+    if (!router.query && isAuthenticated) {
+      router.push("/");
+    }
+  }, [router, isAuthenticated]);
+
+  // If an already logged-in user accesses this page, redirect them home.
+  // This is not rendered based on any side-effect, so don't useEffect() here.
+  if (router.isReady && isAuthenticated) {
+    router.push("/");
+  }
+
+  return (
+    <>
+      <TitleAndMetaTags color="black" title="Log in to your Vault | Vana" />
+      <LayoutAppNoAside>
+        <LayoutCanvas>
+          <LayoutCanvasPattern />
+          <Center tw="min-h-[300px] relative">
+            <Stack tw="gap-5 items-center">
+              <Stack tw="gap-1 items-center">
+                <Text variant="base" weight="semibold">
+                  Log in or create a new account
+                </Text>
+                <Text
+                  variant="note"
+                  tw="text-labelSecondary flex items-center gap-1"
+                >
+                  <WithIcon prefix={<Icon icon="carbon:idea" />}>
+                    If you have an account, use the same login method
+                  </WithIcon>
+                </Text>
+              </Stack>
+              <LoginButton />
+            </Stack>
+          </Center>
+        </LayoutCanvas>
+      </LayoutAppNoAside>
+    </>
+  );
+};
 
 export default LoginPage;
