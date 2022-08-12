1408a25307584350e2d55ee07030bdfef798a52d
Callum Flack <callum@vana.org>
2022-08-12 13:50:19 +1000

    storage instructions copy: generalise it

--- src/data/instructionsCopy.ts
+++ src/data/instructionsCopy.ts
@@ -1,7 +1,7 @@
 const INSTRUCTIONS_INFORMATION = `
 To download your data from this provider, follow the instructions below.
 
-Please note: while the data provider may say it takes anywhere from 48 hours to 2 weeks to deliver your data, it usually takes around 5 minutes to receive their email with your data link.
+Please note: while the data provider may say it takes anywhere from 48 hours to 2 weeks to email you a data download link, it may take as little as 5 minutes.
 
 Any issues? Email [support@vanahelp.zendesk.com](mailto:support@vanahelp.zendesk.com).
 `;
