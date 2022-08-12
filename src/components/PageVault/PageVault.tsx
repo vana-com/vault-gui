0c2ca2d3d8cb86fcad96a06a94474eb96c2d821f
Callum Flack <callum@vana.org>
2022-08-12 16:11:25 +1000

    clean-up PageVault ready for Callum's next PR

--- src/components/PageVault/PageVault.tsx
+++ src/components/PageVault/PageVault.tsx
@@ -2,17 +2,8 @@ import NextLink from "next/link";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from "twin.macro";
 
-import {
-  Button,
-  Card,
-  Container,
-  LoginButton,
-  LogoutButton,
-  NAV_HEIGHT,
-} from "src/components";
+import { Button, Card, Container, NAV_HEIGHT } from "src/components";
 import { CarbonArrowLeft } from "src/components/Icons";
-
-import { useUserContext } from "../UserAccess/UserContext";
 
 /* PageVault is the default page layout + header for a Vault page */
 
@@ -21,46 +12,43 @@ type PageVaultProps = {
   children: React.ReactNode;
 };
 
-const PageVault = ({ children, showBackLink }: PageVaultProps) => {
-  const { user } = useUserContext();
-
-  return (
-    <div tw="relative">
-      {/* HIDDEN FOR A HOT MINUTE — TODO: Callum refactoring shortly based on new UI design */}
-      {showBackLink && (
-        <div tw="fixed top-8 left-8 z-10 hidden">
-          <NextLink passHref href="/">
-            <Button
-              as="a"
-              size="md"
-              variant="outline"
-              prefix={<CarbonArrowLeft />}
-            >
-              Vault
-            </Button>
-          </NextLink>
-        </div>
-      )}
-      <div tw="fixed top-8 right-8 z-10 hidden">
-        {user ? <LogoutButton /> : <LoginButton />}
+const PageVault = ({ children, showBackLink }: PageVaultProps) => (
+  <div tw="relative">
+    {/* HIDDEN FOR A HOT MINUTE — TODO: Callum refactoring shortly based on new UI design */}
+    {showBackLink && (
+      <div tw="fixed top-8 left-8 z-10 hidden">
+        <NextLink passHref href="/">
+          <Button
+            as="a"
+            size="md"
+            variant="outline"
+            prefix={<CarbonArrowLeft />}
+          >
+            Vault
+          </Button>
+        </NextLink>
       </div>
+    )}
+    {/* RETAIN for Callum's refactoring */}
+    {/* <div tw="fixed top-8 right-8 z-10 hidden">
+        {user ? <LogoutButton /> : <LoginButton />}
+      </div> */}
 
-      <Container
-        tw="relative"
-        css={{ paddingTop: `${NAV_HEIGHT * 2}px` }}
-        size="lg"
-      >
-        <div tw="flex flex-col gap-8 relative">
-          {/* <HeaderBasic
+    <Container
+      tw="relative"
+      css={{ paddingTop: `${NAV_HEIGHT * 2}px` }}
+      size="lg"
+    >
+      <div tw="flex flex-col gap-8 relative">
+        {/* <HeaderBasic
             heading="Vault"
             subheading="Unlock the power of your data"
           /> */}
 
-          <Card>{children}</Card>
-        </div>
-      </Container>
-    </div>
-  );
-};
+        <Card>{children}</Card>
+      </div>
+    </Container>
+  </div>
+);
 
 export { PageVault };
