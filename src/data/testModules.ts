d92f0c920abe72f44ddaa36ecb0744cbf9eb88d4
Callum Flack <callum@vana.org>
2022-08-12 10:54:10 +1000

    make testModules data obviously fake
    so we don't forget this task: https://app.clickup.com/t/2tkbzz1

--- src/data/testModules.ts
+++ src/data/testModules.ts
@@ -4,23 +4,23 @@ export const testModules: Module[] = [
   {
     id: "1",
     name: "Google",
-    fileName: "Takeout-20220511T07acs36.zip",
-    lastUpdated: "August 8, 2022",
-    fileSize: "261.4mb",
+    fileName: "FakeFileName-20220511T07acs36.zip",
+    lastUpdated: "FakeDate 8, 2022",
+    fileSize: "Fake.4mb",
   },
   {
     id: "2",
     name: "Facebook",
-    fileName: "Takeout-20220511T07acs36.zip",
-    lastUpdated: "August 8, 2022",
-    fileSize: "261.4mb",
+    fileName: "FakeFileName-20220511T07acs36.zip",
+    lastUpdated: "FakeDate 8, 2022",
+    fileSize: "Fake.4mb",
   },
   {
     id: "3",
     name: "Instagram",
-    fileName: "Takeout-20220511T07acs36.zip",
-    lastUpdated: "August 8, 2022",
-    fileSize: "261.4mb",
+    fileName: "FakeFileName-20220511T07acs36.zip",
+    lastUpdated: "FakeDate 8, 2022",
+    fileSize: "Fake.4mb",
   },
 ];
 
