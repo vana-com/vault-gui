a3cb3daef2d54185c8da99941881a8a97e9a5ba0
Callum Flack <callum@vana.org>
2022-08-17 15:20:26 +1000

    cleanup `Authenticated*` components
    * add router.isReady check

--- src/components/Layout/AuthenticatedComponent.tsx
+++ src/components/Layout/AuthenticatedComponent.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import { useUserContext } from "src/components";
 import { Children } from "src/types";
 
-/* A component that only renders children is the user isAuthenticated */
+/* A component that only renders children if the user isAuthenticated */
 
 const AuthenticatedComponent = ({ children }: Children) => {
   const router = useRouter();
