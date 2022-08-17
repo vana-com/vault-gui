80d172362f16ebc79206f353334654d6bf11ca6a
Callum Flack <callum@vana.org>
2022-08-17 15:10:23 +1000

    create AuthenticatedComponent
    * a component that only renders children is the user isAuthenticated
    * not in use but potentially v useful (until Next.js have a solid Layout system)

---
+++ src/components/Layout/AuthenticatedComponent.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+
+import { useUserContext } from "src/components";
+import { Children } from "src/types";
+
+/* A component that only renders children is the user isAuthenticated */
+
+const AuthenticatedComponent = ({ children }: Children) => {
+  const router = useRouter();
+  const { isAuthenticated } = useUserContext();
+
+  // useEffect(() => {
+  //   if (router.isReady && !isAuthenticated) {
+  //     return undefined;
+  //   }
+  //   return <>{children}</>;
+  // }, [isAuthenticated]);
+
+  if (router.isReady && !isAuthenticated) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export { AuthenticatedComponent };
