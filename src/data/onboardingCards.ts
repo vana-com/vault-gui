978a8faa2625430f3fea76be611937b329a7a80b
Callum Flack <callum@vana.org>
2022-08-25 18:14:44 +1000

    Update onboardingCards.ts

--- src/data/onboardingCards.ts
+++ src/data/onboardingCards.ts
@@ -4,11 +4,11 @@ Now you can use your data in ways you didn’t think you could.`;
 
 const DESCRIPTION_SECURITY = `We lock it. Properly. Your Vault is cryptographically secured.
 
-Only you can unlock it. We don’t have access to read your data.`;
+Only you can unlock it. We cannot read your data.`;
 
-const DESCRIPTION_AGENCY = `Share your data where and when you want to, with confidence.
+const DESCRIPTION_AGENCY = `Share your data where and when you want to.
 
-When you do, apps are given only the data you’ve selected. The sharing connection is generated then destroyed on-the-fly.`;
+When you do, apps are given only the data you chose. The sharing connection is generated then destroyed on-the-fly.`;
 
 export const onboardingCards = [
   {
