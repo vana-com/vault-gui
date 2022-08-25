2bcb29c33a06833738524bad64e5dcd27e6bbf5f
Callum Flack <callum@vana.org>
2022-08-25 12:17:07 +1000

    delete DialogModalAdd in favour of DialogModal

--- src/components/Dialogs/index.ts
+++ src/components/Dialogs/index.ts
@@ -4,7 +4,6 @@ export * from "./DialogDrawer";
 export * from "./DialogDrawer2";
 export * from "./DialogDrawerControlled";
 export * from "./DialogModal";
-export * from "./DialogModalAdd";
 export * from "./DialogModalControlled";
 export * from "./Drawer.css";
 export * from "./Trigger.css";
