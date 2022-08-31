ca4cb963495c3de86fa740ddca7b7b3c505c66fe
Joseph Csoti <joseph@vana.org>
2022-08-31 11:59:35 -0400

    more events

--- src/config/heap.ts
+++ src/config/heap.ts
@@ -4,6 +4,12 @@ enum HEAP_EVENTS {
   SHARE_APPROVED = "Share Approved",
   SHARE_CANCELLED = "Share Cancelled",
   CLICK_ADD_DATA = "Click Add Data",
+  CLICK_ADD_GOOGLE = "Click Add Google Data",
+  CLICK_ADD_FACEBOOK = "Click Add Facebook Data",
+  CLICK_ADD_INSTAGRAM = "Click Add Instagram",
+  CLICK_DELETE_VAULT = "Click Delete Vault",
+  CLICK_DATA_DELETE_MODAL_CANCEL = "Click Cancel Data Delete Modal",
+  CLICK_DATA_DELETE_MODAL_CONFIRM = "Click Cancel Data Delete Modal",
   DATA_STORED = "Data Stored",
   DATA_DELETED = "Data Deleted",
 }
