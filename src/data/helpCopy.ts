c59a91ef6a0efdc0cc85e86b6adeb59c2f457eb2
Callum Flack <callum@vana.org>
2022-09-05 09:14:46 +1000

    HelpCopy: delete unused HELP_INFORMATION_WITH_VIDEO

--- src/data/helpCopy.ts
+++ src/data/helpCopy.ts
@@ -2,6 +2,4 @@ import config from "src/config";
 
 const HELP_INFORMATION = `If you encounter any issues, email please email [${config.vanaSupportEmail}](mailto:${config.vanaSupportEmail}) and we'll respond as soon as we're able.`;
 
-const HELP_INFORMATION_WITH_VIDEO = `Here is a [video walkthrough](https://www.youtube.com/watch?v=uDosRHMh37Y). If you encounter any issues, please email [support@vanahelp.zendesk.com](mailto:support@vanahelp.zendesk.com) (preferably with a screenshot of your issue) and we'll help you resolve the issue within 24 hours.`;
-
-export { HELP_INFORMATION, HELP_INFORMATION_WITH_VIDEO };
+export { HELP_INFORMATION };
