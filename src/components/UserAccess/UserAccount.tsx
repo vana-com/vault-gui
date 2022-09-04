0983ce0a5c34fb17893d19affc7a1777025b8adb
Callum Flack <callum@vana.org>
2022-09-05 08:23:54 +1000

    Fix user account login name format

--- src/components/UserAccess/UserAccount.tsx
+++ src/components/UserAccess/UserAccount.tsx
@@ -8,41 +8,46 @@ import {
   styledNavButton,
   Text,
 } from "src/components";
+import { capitalizeString } from "src/utils";
 
 interface Props {
   user: any;
-  accountLoginService?: string;
+  accountLoginService: string;
 }
 
-const UserAccount = ({ user, accountLoginService }: Props) => (
-  <Stack tw="gap-2 md:min-w-[300px] pb-2 md:pb-0">
-    <Group tw="px-3 py-4 gap-3 items-center cursor-auto">
-      <div css={[styledNavButton, tw`h-[55px] w-[55px]`]}>
-        <Icon icon="carbon:user-avatar-filled-alt" height="3em" />
-      </div>
-      <Stack tw="gap-0.5">
-        {accountLoginService && (
-          <Text variant="note" weight="medium" tw="flex">
-            Login:
-            <Text variant="note" weight="semibold" tw="pl-1">
-              {accountLoginService[0].toUpperCase() +
-                accountLoginService.slice(1)}
+const UserAccount = ({ user, accountLoginService }: Props) => {
+  const prettyServiceName = capitalizeString(
+    accountLoginService.replaceAll("-", " "),
+  );
+
+  return (
+    <Stack tw="gap-2 md:min-w-[300px] pb-2 md:pb-0">
+      <Group tw="px-3 py-4 gap-3 items-center cursor-auto">
+        <div css={[styledNavButton, tw`h-[55px] w-[55px]`]}>
+          <Icon icon="carbon:user-avatar-filled-alt" height="3em" />
+        </div>
+        <Stack tw="gap-0.5">
+          {accountLoginService && (
+            <Text variant="note" weight="medium" tw="flex">
+              Login:
+              <Text variant="note" weight="semibold" tw="pl-1">
+                {prettyServiceName}
+              </Text>
+            </Text>
+          )}
+          <Text variant="footnote" weight="medium" tw="text-labelTertiary">
+            ID ends in{" "}
+            <Text as="span" tw="font-mono">
+              {user.id.slice(-8)}
             </Text>
           </Text>
-        )}
-        <Text variant="footnote" weight="medium" tw="text-labelTertiary">
-          ID ends in{" "}
-          <Text as="span" tw="font-mono">
-            {user.id.slice(-8)}
-          </Text>
-        </Text>
-      </Stack>
-    </Group>
-    <hr tw="border-separatorLight" />
-    <Stack tw="gap-[2px]">
-      <LogoutButton />
-      {/* TODO: reinstate when delete account function is ready */}
-      {/* <hr tw="border-separatorLight" />
+        </Stack>
+      </Group>
+      <hr tw="border-separatorLight" />
+      <Stack tw="gap-[2px]">
+        <LogoutButton />
+        {/* TODO: reinstate when delete account function is ready */}
+        {/* <hr tw="border-separatorLight" />
       <Text
         as="button"
         variant="base"
@@ -54,8 +59,9 @@ const UserAccount = ({ user, accountLoginService }: Props) => (
           Delete Vault
         </WithIcon>
       </Text> */}
+      </Stack>
     </Stack>
-  </Stack>
-);
+  );
+};
 
 export { UserAccount };
