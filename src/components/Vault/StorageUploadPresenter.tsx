ec52eef5734c29037dcd8db68a63af20321d7202
Callum Flack <callum@vana.org>
2022-09-05 14:38:14 +1000

    Ensure Vault go live (CU-3de0qn4) (#95)
    * Fix user account login name format
    
    * getFileSizeString: remove space between value and byte
    
    * Update help copy
    
    See comments in https://app.clickup.com/t/3t4w4tn
    
    * HelpCopy: delete unused HELP_INFORMATION_WITH_VIDEO
    
    * StorageUploadPresenter: improve layout styles
    
    * fix preload font path

--- src/components/Vault/StorageUploadPresenter.tsx
+++ src/components/Vault/StorageUploadPresenter.tsx
@@ -26,7 +26,7 @@ interface Props {
   filesToUploadDescription: string;
 }
 
-const boxStyle = tw`relative flex items-center justify-center w-full h-full text-black rounded p-insetHalf bg-neutral min-h-[160px]`;
+const boxStyle = tw`relative flex items-center justify-center flex-1 w-full h-full text-black rounded p-insetHalf bg-neutral`;
 
 const boxHoverStyle = tw`border border-dashed border-labelQuaternary hover:(border-primary)`;
 
@@ -41,99 +41,93 @@ const StorageUploadPresenter = ({
   uploadProgress,
   filesToUploadDescription,
 }: Props) => (
-  <>
-    <div
-      tw="cursor-default w-full relative"
-      onDrop={onDropFile}
-      onDragOver={(event) => {
-        // Prevent default behavior (prevent file from being opened)
-        event.preventDefault();
-      }}
-    >
-      {/* INSTRUCTIONS top right inner corner: DEPRECATED */}
-      {/* <div tw="absolute top-1 right-1 z-10">{instructionsNode}</div> */}
+  // This outer div has min-height set. The internal content block for each step occupies the remaining space by using `flex-1`.
+  <div
+    tw="cursor-default w-full relative flex flex-col min-h-[160px]"
+    onDrop={onDropFile}
+    onDragOver={(event) => {
+      // Prevent default behavior (prevent file from being opened)
+      event.preventDefault();
+    }}
+  >
+    {/* INSTRUCTIONS top right inner corner: DEPRECATED */}
+    {/* <div tw="absolute top-1 right-1 z-10">{instructionsNode}</div> */}
 
-      {/* FILE INPUT */}
-      <div tw="sr-only">
-        <FileInput onChange={handleSelectFile} />
-      </div>
+    {/* FILE INPUT */}
+    <div tw="sr-only">
+      <FileInput onChange={handleSelectFile} />
+    </div>
 
-      {/* STEP 1: DROP */}
-      {!isDataUploading && filesToUpload.length === 0 && (
-        <Button
-          variant="contrast"
-          isDisabled={isDataUploading}
-          onClick={openFileDialog}
-          css={[boxStyle, boxHoverStyle]}
-        >
-          <Stack tw="items-center gap-0.5">
-            <Icon icon="carbon:upload" height="1.75em" />
-            <div tw="pt-1">
-              <Text color="black" weight="semibold" variant="body">
-                Drop file(s) here or click to select
-              </Text>
-            </div>
-            <Text variant="note" tw="text-labelSecondary">
-              {filesToUploadDescription}
+    {/* STEP 1: DROP */}
+    {!isDataUploading && filesToUpload.length === 0 && (
+      <Button
+        variant="contrast"
+        isDisabled={isDataUploading}
+        onClick={openFileDialog}
+        css={[boxStyle, boxHoverStyle]}
+      >
+        <Stack tw="items-center gap-0.5">
+          <Icon icon="carbon:upload" height="1.75em" />
+          <div tw="pt-1">
+            <Text color="black" weight="semibold" variant="body">
+              Drop file(s) here or click to select
             </Text>
-          </Stack>
-        </Button>
-      )}
-
-      {/* STEP 2: CONFIRM */}
-      {!isDataUploading && !!filesToUpload.length && (
-        <Stack tw="gap-3 transform lg:translate-y-2" css={boxStyle}>
-          {filesToUpload.map((fileToUpload, i) => (
-            <Stack
-              key={fileToUpload.toString()}
-              tw="gap-1 items-center transform translate-y-1.5"
-            >
-              <Group tw="gap-1 justify-center items-center">
-                <Text color="black" weight="semibold" variant="body">
-                  {getAbbreviatedFileName(fileToUpload.name)}
-                </Text>
-                <TooltipDefault
-                  label="Remove file"
-                  align="center"
-                  side="right"
-                  sideOffset={0}
-                >
-                  <Button
-                    aria-label="Remove file to upload"
-                    variant="icon"
-                    tw="text-black"
-                    type="reset"
-                    onClick={() => {
-                      const copyFilesToUpload = [...filesToUpload];
-                      copyFilesToUpload.splice(i, 1);
-                      setFilesToUpload(copyFilesToUpload);
-                    }}
-                  >
-                    <Icon icon="carbon:close-filled" />
-                  </Button>
-                </TooltipDefault>
-              </Group>
-            </Stack>
-          ))}
-          <Text
-            variant="note"
-            tw="text-primary font-medium flex items-center gap-1"
-          >
-            <WithIcon
-              prefix={
-                <Icon icon="carbon:checkmark" height="1.25em" tw="mt-px" />
-              }
-            >
-              Looks good
-            </WithIcon>
+          </div>
+          <Text variant="note" tw="text-labelSecondary">
+            {filesToUploadDescription}
           </Text>
         </Stack>
-      )}
+      </Button>
+    )}
 
-      {/* STEP 3: UPLOAD */}
-      {isDataUploading && <StorageProgress storeProgress={uploadProgress} />}
-    </div>
-  </>
+    {/* STEP 2: CONFIRM */}
+    {!isDataUploading && !!filesToUpload.length && (
+      <Stack tw="gap-2 transform lg:translate-y-1.5" css={boxStyle}>
+        {filesToUpload.map((fileToUpload, i) => (
+          <Stack key={fileToUpload.toString()} tw="gap-1 items-center">
+            <Group tw="gap-1 justify-center items-center">
+              <Text color="black" weight="semibold" variant="body">
+                {getAbbreviatedFileName(fileToUpload.name)}
+              </Text>
+              <TooltipDefault
+                label="Remove file"
+                align="center"
+                side="right"
+                sideOffset={0}
+              >
+                <Button
+                  aria-label="Remove file to upload"
+                  variant="icon"
+                  tw="text-black"
+                  type="reset"
+                  onClick={() => {
+                    const copyFilesToUpload = [...filesToUpload];
+                    copyFilesToUpload.splice(i, 1);
+                    setFilesToUpload(copyFilesToUpload);
+                  }}
+                >
+                  <Icon icon="carbon:close-filled" />
+                </Button>
+              </TooltipDefault>
+            </Group>
+          </Stack>
+        ))}
+        <Text
+          variant="note"
+          tw="text-primary font-medium flex items-center gap-1"
+        >
+          <WithIcon
+            prefix={<Icon icon="carbon:checkmark" height="1.25em" tw="mt-px" />}
+          >
+            Looks good
+          </WithIcon>
+        </Text>
+      </Stack>
+    )}
+
+    {/* STEP 3: UPLOAD */}
+    {isDataUploading && <StorageProgress storeProgress={uploadProgress} />}
+  </div>
 );
 
 export { StorageUploadPresenter };
