55d66063218857120ceef613c4dd835f598626a4
Callum Flack <callum@vana.org>
2022-09-15 17:16:03 +1000

    NavAsideBetaTypeform: revise
    * brighter image
    * remove icon

--- src/components/Nav/NavAsideBetaTypeform.tsx
+++ src/components/Nav/NavAsideBetaTypeform.tsx
@@ -1,4 +1,3 @@
-import { Icon } from "@iconify/react";
 import { PopupButton } from "@typeform/embed-react";
 import Image from "next/image";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -10,7 +9,6 @@ import {
   styledNavLinkHover,
   Text,
   useUserContext,
-  WithIcon,
 } from "src/components";
 import config from "src/config";
 import { heapTrackServerSide } from "src/utils";
@@ -30,7 +28,7 @@ const NavAsideBetaTypeform = () => {
     >
       <Flex css={styledNavLinkHover} tw="items-center justify-end relative">
         <Image
-          src="/images/vana-keys-beta.png"
+          src="/images/aside-feedback.png"
           layout="intrinsic"
           width="100"
           height="100"
@@ -50,11 +48,12 @@ const NavAsideBetaTypeform = () => {
                 heapTrackServerSide(user?.id, HEAP_EVENTS.CLICK_GIVE_FEEDBACK)
               }
             >
-              <WithIcon
+              Give feedback
+              {/* <WithIcon
                 suffix={<Icon icon="carbon:arrow-up-right" rotate="0deg" />}
               >
                 Give feedback
-              </WithIcon>
+              </WithIcon> */}
             </Text>
           </Stack>
         </Flex>
