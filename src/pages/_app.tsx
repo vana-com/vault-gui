7f069b202a01bc88a2d32ebd95e6ba38c2e18aca
Zach Hay <zach@vana.org>
2022-09-30 18:02:14 -0700

    Setup Datadog + Zendesk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/fonts.css";
 
 import { ApolloProvider } from "@apollo/client";
+import { datadogRum } from "@datadog/browser-rum";
 import { cache } from "@emotion/css";
 import { CacheProvider } from "@emotion/react";
 import * as Toast from "@radix-ui/react-toast";
 import * as Tooltip from "@radix-ui/react-tooltip";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from "twin.macro";
@@ -23,6 +25,21 @@ import { useApollo } from "../utils/apolloClient";
 const NextApp = ({ Component, pageProps }: AppProps) => {
   const client = useApollo(pageProps);
 
+  // Datadog RUM initialization
+  datadogRum.init({
+    applicationId: process.env.NEXT_PUBLIC_DATADOG_APP_ID as string,
+    clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN as string,
+    site: "datadoghq.com",
+    service: "vault-gui",
+    env: process.env.NEXT_PUBLIC_ENV as string,
+    version: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA as string,
+    sampleRate: 100,
+    premiumSampleRate: 0,
+    trackInteractions: true,
+    defaultPrivacyLevel: "mask-user-input",
+  });
+  datadogRum.startSessionReplayRecording();
+
   return (
     <ApolloProvider client={client}>
       <CacheProvider value={cache}>
@@ -35,6 +52,12 @@ const NextApp = ({ Component, pageProps }: AppProps) => {
                   <AppHook>
                     <LayoutApp renderNavMobile>
                       <Component {...pageProps} />
+                      {/** Start of vanahelp Zendesk Widget script */}
+                      <Script
+                        id="ze-snippet"
+                        src="https://static.zdassets.com/ekr/snippet.js?key=ae33c673-67c2-4bb9-96a4-3b62821d9567"
+                      />
+                      {/** End of vanahelp Zendesk Widget script */}
                     </LayoutApp>
                   </AppHook>
                 </AuthenticatedLayout>
