e29e9174385fac9dc7c0d328f9815dd427735d0b
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2022-10-18 17:51:16 -0700

    feat: add signed url for images

--- src/pages/api/[user-email]/gallery/[id]/exhibit/[exhibit-name].ts
+++ src/pages/api/[user-email]/gallery/[id]/exhibit/[exhibit-name].ts
@@ -1,5 +1,8 @@
+import { GetSignedUrlConfig } from "@google-cloud/storage";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import config from "src/config";
+import serverConfig from "src/config/server";
 import { Exhibit } from "src/types/exhibit";
 import { readGCSDirectory } from "src/utils/serverUtils";
 
@@ -39,11 +42,29 @@ const formatExhibitName = (exhibitName: string) => {
   return name.charAt(0).toUpperCase() + name.slice(1);
 };
 
+const getSignedUrl = async (fileName: string) => {
+  const options = {
+    version: "v4",
+    action: "read",
+    expires: Date.now() + config.preSignedObjectURLTTLInMilliseconds,
+  } as GetSignedUrlConfig;
+
+  // Get a v4 signed URL for reading the file
+  const [signedUrl] = await serverConfig.userDataBucket
+    .file(fileName)
+    .getSignedUrl(options);
+
+  return signedUrl;
+};
+
 export const getExhibit = async (exhibitKey: string): Promise<Exhibit> => {
   const [files] = await readGCSDirectory(exhibitKey);
   const exhibitName = exhibitKey.split("/").pop();
 
-  const images = files.map((f) => f.name);
+  const fileNames = files.map((f) => f.name);
+  const images = await Promise.all(
+    fileNames.map(async (fileName: string) => getSignedUrl(fileName)),
+  );
 
   return {
     name: formatExhibitName(exhibitName as string),
