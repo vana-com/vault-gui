2b1f67154319e5b8b11afdd5204ed81e0a6f87f3
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2022-10-18 17:10:23 -0700

    refactor: use getExhibit function to fetch all exhibits in gallery endpoint

--- src/pages/api/[user-email]/gallery/[id].ts
+++ src/pages/api/[user-email]/gallery/[id].ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import { Exhibit } from "src/types/exhibit";
 import { readGCSDirectory } from "src/utils/serverUtils";
+
+import { getExhibit } from "./[id]/exhibit/[exhibit-name]";
 
 export default async (
   req: NextApiRequest,
@@ -12,19 +15,20 @@ export default async (
   console.log("user-email:", userEmail);
 
   const galleryId = `gallery-${id}`;
-  const key = `${userEmail}/${galleryId}`;
+  const keyPrefix = `${userEmail}/${galleryId}`;
 
-  const [files] = await readGCSDirectory(key);
+  const [files] = await readGCSDirectory(keyPrefix);
 
   const fileNames = files.map((f) => f.name);
   const exhibits = getExhibitNames(fileNames);
+  const exhibitKeys = createExhibitKeys(exhibits, keyPrefix);
 
   const response = {
     id: galleryId,
-    exhibits,
+    exhibits: await getExhibits(exhibitKeys),
   };
 
-  console.log(JSON.stringify(response, "", 2));
+  console.log(JSON.stringify(response, null, 2));
   res.setHeader("Cache-Control", "public, max-age=3600");
   return res.status(200).json(response);
 };
@@ -38,3 +42,9 @@ const getExhibitNames = (files: string[]): string[] => {
   const exhibitsSet = new Set(exhibits);
   return Array.from(exhibitsSet);
 };
+
+const getExhibits = async (exhibitKeys: string[]): Promise<Exhibit[]> =>
+  Promise.all(exhibitKeys.map(async (key: string) => getExhibit(key)));
+
+const createExhibitKeys = (exhibits: string[], keyPrefix: string): string[] =>
+  exhibits.map((exhibit: string) => `${keyPrefix}/${exhibit}`);
