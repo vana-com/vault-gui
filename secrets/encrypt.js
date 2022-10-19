5693a11526fd60f174cdd933d93e4a499b7827a4
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2022-10-18 19:52:33 -0700

    refactor: add additional encoding argument to encrypt script

--- secrets/encrypt.js
+++ secrets/encrypt.js
@@ -6,7 +6,7 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env.local") });
 /**
  * Utility to encrypt a string, to be used in local secrets store
  */
-const encryptSecret = (value) => {
+const encryptSecret = (value, encoding) => {
   console.log("Value to encrypt: ", value);
 
   const cipher = crypto.createCipheriv(
@@ -15,14 +15,16 @@ const encryptSecret = (value) => {
     process.env.SECRET_ENCRYPTION_IV,
   );
 
-  let encrypted = cipher.update(value, "utf8", "base64");
-  encrypted += cipher.final("base64");
+  let encrypted = cipher.update(value, "utf8", encoding);
+  encrypted += cipher.final(encoding);
 
   console.log("Encrypted value: ", encrypted);
 };
 
 if (process.argv.length === 3) {
-  encryptSecret(process.argv[2]);
+  encryptSecret(process.argv[2], "base64");
+} else if (process.argv.length === 4) {
+  encryptSecret(process.argv[2], process.argv[3]);
 } else {
   console.error(`Usage: yarn encrypt "secret"`);
 }
