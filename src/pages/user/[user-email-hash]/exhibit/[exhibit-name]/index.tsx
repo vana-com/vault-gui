c1299cefea93698842fae066b00f36e5a77aca6e
Callum Flack <callum@vana.org>
2022-10-25 10:13:50 -0700

    gallery type: update sliced name

--- src/pages/user/[user-email-hash]/exhibit/[exhibit-name]/index.tsx
+++ src/pages/user/[user-email-hash]/exhibit/[exhibit-name]/index.tsx
@@ -90,7 +90,7 @@ const ExhibitPage: NextPage = () => {
       <div className="flex items-center justify-center h-screen">
         <div role="status">
           <svg
-            className="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -185,7 +185,7 @@ const ExhibitPage: NextPage = () => {
             }
           >
             <p className="text-stone-400">
-              {exhibit.name} images from Gallery {userEmailHash?.slice(-7)}.
+              {exhibit.name} images from Gallery {userEmailHash?.slice(-4)}.
               {/* TODO: add share button here… */}
             </p>
           </PageHeading>
