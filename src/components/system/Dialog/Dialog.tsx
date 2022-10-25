e8ef87e909300b02ef3f4118725f7923024623e5
Callum Flack <callum@vana.org>
2022-10-25 11:55:47 -0700

    gallery exhibit: refactor dialog

--- src/components/system/Dialog/Dialog.tsx
+++ src/components/system/Dialog/Dialog.tsx
@@ -11,6 +11,7 @@ interface Props
     DialogPrimitive.DialogContentProps {
   overlayClassName?: string;
   onOpenChange?: (isOpen: boolean) => void;
+  showCloseIcon?: boolean;
 }
 
 const Dialog = ({
@@ -19,6 +20,8 @@ const Dialog = ({
   onOpenChange,
   overlayClassName = "max-w-md",
   contentClassName = "max-w-md",
+  showCloseIcon,
+  onPointerDownOutside,
 }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   // const hey = true;
@@ -33,54 +36,65 @@ const Dialog = ({
       defaultOpen
     >
       <DialogPrimitive.Trigger asChild>{triggerNode}</DialogPrimitive.Trigger>
-      <Transition.Root show={isOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <DialogPrimitive.Overlay
-            forceMount
-            className={clsx("fixed inset-0 z-20 bg-black/50", overlayClassName)}
-          />
-        </Transition.Child>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <DialogPrimitive.Content
-            forceMount
-            className={clsx(
-              "fixed z-50",
-              "w-[90vw] rounded-lg lg:w-full",
-              "top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]",
-              "bg-white dark:bg-gray-800",
-              "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
-              contentClassName,
-            )}
+      <DialogPrimitive.Portal>
+        <Transition.Root show={isOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            {children}
-
-            <DialogPrimitive.Close
+            <DialogPrimitive.Overlay
+              forceMount
               className={clsx(
-                "absolute top-3.5 right-3.5 inline-flex items-center justify-center rounded-full p-1",
+                "fixed inset-0 z-20 bg-black/50",
+                overlayClassName,
+              )}
+            />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <DialogPrimitive.Content
+              forceMount
+              // Don't close the Dialog when clicking outside
+              // onPointerDownOutside={() => setIsOpen(false)}
+              // onPointerDownOutside={(event) => event.preventDefault()}
+              onPointerDownOutside={onPointerDownOutside}
+              className={clsx(
+                "fixed z-50",
+                "w-[90vw] rounded-lg lg:w-full",
+                "top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]",
+                "bg-white dark:bg-gray-800",
                 "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
+                contentClassName,
               )}
             >
-              <Cross1Icon className="w-4 h-4 text-gray-500 hover:text-gray-700 dark:text-gray-500 dark:hover:text-gray-400" />
-            </DialogPrimitive.Close>
-          </DialogPrimitive.Content>
-        </Transition.Child>
-      </Transition.Root>
+              {children}
+
+              {showCloseIcon && (
+                <DialogPrimitive.Close
+                  className={clsx(
+                    "absolute top-3.5 right-3.5 inline-flex items-center justify-center rounded-full p-1",
+                    "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
+                  )}
+                >
+                  <Cross1Icon className="w-4 h-4 text-gray-500 hover:text-gray-700 dark:text-gray-500 dark:hover:text-gray-400" />
+                </DialogPrimitive.Close>
+              )}
+            </DialogPrimitive.Content>
+          </Transition.Child>
+        </Transition.Root>
+      </DialogPrimitive.Portal>
     </DialogPrimitive.Root>
   );
 };
