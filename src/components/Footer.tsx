a23c28101cce1c6957e0d2a99d037310dbfbfdf4
Callum Flack <callum@vana.org>
2022-11-02 09:55:56 +1000

    Footer: make social links target blank

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { Icon } from "@iconify/react";
 import clsx from "clsx";
-import Link from "next/link";
 
 import config from "src/config";
 
@@ -12,34 +11,34 @@ interface Props {
 const Footer = ({ blackTheme, wrapperClassName = "pb-w72" }: Props) => (
   <footer className={clsx("flex justify-center", wrapperClassName)}>
     <div className={clsx(blackTheme ? "text-white" : "text-stone-500")}>
-      {/* <hr className="hr hr-vertical h-[12px] opacity-50" /> */}
       <div className="flex items-center justify-center gap-3 text-sm">
-        <Link href={config.vanaInstagram} passHref>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="">
-            <span className="hidden">Instagram</span>
-            <Icon icon="carbon:logo-instagram" height="2.25em" />
-          </a>
-        </Link>
-        <Link href={config.vanaDiscord} passHref>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a>
-            <span className="hidden">Discord</span>
-            <Icon icon="carbon:logo-discord" height="2.25em" />
-          </a>
-        </Link>
+        <a
+          href={config.vanaInstagram}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="hidden">Instagram</span>
+          <Icon icon="carbon:logo-instagram" height="2.25em" />
+        </a>
+        <a href={config.vanaDiscord} target="_blank" rel="noopener noreferrer">
+          <span className="hidden">Discord</span>
+          <Icon icon="carbon:logo-discord" height="2.25em" />
+        </a>
         <a href={`mailto:${config.vanaSupportEmail}`}>
           <span className="hidden">Support</span>
           <Icon icon="carbon:email" height="2.25em" />
         </a>
       </div>
 
-      {/* tag search link */}
+      {/* IG TAG LINK */}
       <div className="pt-2.5 text-sm text-center">
-        <Link href={config.vanaInstagramHashSearch} passHref>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a>#vanaportrait</a>
-        </Link>
+        <a
+          href={config.vanaInstagramHashSearch}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          #vanaportrait
+        </a>
       </div>
     </div>
   </footer>
