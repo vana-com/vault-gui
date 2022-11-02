043cb2106ed3eed66e248a0c9524908f05ccaf0b
Joseph Csoti <josephcsoti@gmail.com>
2022-11-02 13:47:53 -0400

    Add Google Tag Tracking (#18)
    add google tag tracking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/no-danger */
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 import config from "src/config";
 
@@ -7,6 +9,18 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* <!-- Google Tag Manager --> */}
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-55HTQ34');
+          `}
+          </Script>
+          {/* <!-- End Google Tag Manager --> */}
+
           {/* Start Heap Tag */}
           <script
             dangerouslySetInnerHTML={{
@@ -20,6 +34,13 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-55HTQ34" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* <!-- End Google Tag Manager (noscript) --> */}
         </body>
       </Html>
     );
